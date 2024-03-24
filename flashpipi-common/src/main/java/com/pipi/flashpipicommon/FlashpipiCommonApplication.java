d112927ce7b4433482329013c5525cd6f1e07d96
guo_x <guo_x0315@163.com>
2024-03-24 20:58:42 +0800

    更改了项目目录结构

---
+++ flashpipi-common/src/main/java/com/pipi/flashpipicommon/FlashpipiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.pipi.flashpipicommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashpipiCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashpipiCommonApplication.class, args);
+	}
+
+}
---
+++ flashpipi-common/src/test/java/com/pipi/flashpipicommon/FlashpipiCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipi.flashpipicommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashpipiCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
