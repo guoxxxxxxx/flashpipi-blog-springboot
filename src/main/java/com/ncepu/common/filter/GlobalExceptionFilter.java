e30442ed367ace03dec3f5c31b4d2283f43ec39b
guo_x <guo_x0315@163.com>
2024-08-16 15:31:16 +0800

    添加：全局异常捕获

---
+++ src/main/java/com/ncepu/common/filter/GlobalExceptionFilter.java
@@ -0,0 +1,27 @@
+/**
+ * @Time: 2024/8/16 15:21
+ * @Author: guoxun
+ * @File: GlobalExceptionFilter
+ * @Description:
+ */
+
+package com.ncepu.common.filter;
+
+import com.ncepu.common.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionFilter{
+
+    @ExceptionHandler({Exception.class})
+    public CommonResult<Object> handleException(Exception e){
+        log.error(e.toString());
+        return new CommonResult<>().fail().message(e.toString());
+    }
+
+
+}
