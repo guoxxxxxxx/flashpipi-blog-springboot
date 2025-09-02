216a53960a4cef1dfe7e738b88c61ac93ee04985
guox <guo_x0315@163.com>
2025-09-02 16:41:33 +0800

    feat(database): 从单机 MySQL 切换到 ShardingSphere 分片集群
    - 更新 application.yml，使用 ShardingSphere 数据源替换原有 MySQL 数据源
    - 修改 Docker Compose 配置，设置 MySQL 分片集群和 MySQL Router
    - 在项目中集成 ShardingSphere JDBC核心库
    - 新增 shardingsphere-blog.yaml 配置文件，定义数据源和分片规则
    - 调整 Spring Boot 应用以支持 ShardingSphere

--- src/main/java/com/ncepu/FlashpipiBlogSpringbootApplication.java
+++ src/main/java/com/ncepu/FlashpipiBlogSpringbootApplication.java
@@ -3,6 +3,8 @@ package com.ncepu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class FlashpipiBlogSpringbootApplication {
 
